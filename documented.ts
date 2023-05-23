9b8f7733a5c3c178601c45a76b0571e28f4cfa7e
pewpewnor <newpatchnotworking2@tutanota.com>
2023-05-23 23:47:16 +0700

    feat: avoid object.entries caused error

--- documented.ts
+++ documented.ts
@@ -1,64 +1,65 @@
 import { twMerge } from "tailwind-merge";
 
 interface Variant {
-  default: string;
-  [key: string]: string;
+	default: string;
+	[key: string]: string;
 }
 
 interface CreatedVariants {
-  [key: string]: Variant;
+	[key: string]: Variant;
 }
 
 interface ChosenVariants {
-  [key: string]: string;
+	[key: string]: string;
 }
 
 class Variance {
-  private always: string;
-  private createdVariants: CreatedVariants;
+	private always: string;
+	private createdVariants: CreatedVariants;
 
-  constructor(always: string, createdVariants: CreatedVariants) {
-    this.always = always;
-    this.createdVariants = createdVariants;
-  }
+	constructor(always: string, createdVariants: CreatedVariants) {
+		this.always = always;
+		this.createdVariants = createdVariants;
+	}
 
-  public getVariant(chosenVariants: ChosenVariants) {
-    // Throw error if any of the chosen variant type does not exist in created variants
+	public getVariant(chosenVariants: ChosenVariants) {
+		// Throw error if any of the chosen variant type does not exist in created variants
 
-    const chosenVariantKeys = Object.keys(chosenVariants);
-    for (const chosenVariantType of chosenVariantKeys) {
-      if (!(chosenVariantType in this.createdVariants)) {
-        throw new Error(
-          `Error: chosen variant type "${chosenVariantType}" does not exist. File: variance.ts, Line: 31`
-        );
-      }
-    }
+		const chosenVariantKeys = Object.keys(chosenVariants);
+		for (const chosenVariantType of chosenVariantKeys) {
+			if (!(chosenVariantType in this.createdVariants)) {
+				throw new Error(
+					`Error: chosen variant type "${chosenVariantType}" does not exist. File: variance.ts, Line: 31`
+				);
+			}
+		}
 
-    const result = [
-      this.always,
-      // Loop for each created variants
+		const result = [
+			this.always,
+			// Loop for each created variants
 
-      ...Object.entries(this.createdVariants).map(([variantType, variant]) => {
-        // If user specify this variant type
+			...Object.keys(this.createdVariants).map((variantType) => {
+				const variant = this.createdVariants[variantType];
+				// If user specify this variant type
 
-        if (variantType in chosenVariants) {
-          const chosenVariantKey = chosenVariants[variantType];
-          if (chosenVariantKey in variant) {
-            return variant[chosenVariantKey];
-          } else {
-            throw new Error(
-              `Error: chosen variant key "${chosenVariantKey}" does not exist in variant type "${variantType}". File: variance.ts, Line: 50`
-            );
-          }
-        }
-        // If user did not specify this variant type, use the default property value
+				if (variantType in chosenVariants) {
+					const chosenVariantKey = chosenVariants[variantType];
+					if (chosenVariantKey in variant) {
+						return variant[chosenVariantKey];
+					} else {
+						throw new Error(
+							`Error: chosen variant key "${chosenVariantKey}" does not exist in variant type "${variantType}". File: variance.ts, Line: 50`
+						);
+					}
+				}
+				// If user did not specify this variant type, use the default property value
 
-        return variant.default;
-      }),
-    ];
+				return variant.default;
+			}),
+		];
 
-    return twMerge(result.join(" "));
-  }
+		return twMerge(result.join(" "));
+	}
 }
 
 // Creation process syntax:
--- variance.ts
+++ variance.ts
@@ -1,54 +1,55 @@
 import { twMerge } from "tailwind-merge";
 
 interface Variant {
-  default: string;
-  [key: string]: string;
+	default: string;
+	[key: string]: string;
 }
 
 interface CreatedVariants {
-  [key: string]: Variant;
+	[key: string]: Variant;
 }
 
 interface ChosenVariants {
-  [key: string]: string;
+	[key: string]: string;
 }
 
 class Variance {
-  private always: string;
-  private createdVariants: CreatedVariants;
+	private always: string;
+	private createdVariants: CreatedVariants;
 
-  constructor(always: string, createdVariants: CreatedVariants) {
-    this.always = always;
-    this.createdVariants = createdVariants;
-  }
+	constructor(always: string, createdVariants: CreatedVariants) {
+		this.always = always;
+		this.createdVariants = createdVariants;
+	}
 
-  public getVariant(chosenVariants: ChosenVariants) {
-    const chosenVariantKeys = Object.keys(chosenVariants);
-    for (const chosenVariantType of chosenVariantKeys) {
-      if (!(chosenVariantType in this.createdVariants)) {
-        throw new Error(
-          `Error: chosen variant type "${chosenVariantType}" does not exist. File: variance.ts, Line: 31`
-        );
-      }
-    }
+	public getVariant(chosenVariants: ChosenVariants) {
+		const chosenVariantKeys = Object.keys(chosenVariants);
+		for (const chosenVariantType of chosenVariantKeys) {
+			if (!(chosenVariantType in this.createdVariants)) {
+				throw new Error(
+					`Error: chosen variant type "${chosenVariantType}" does not exist. File: variance.ts, Line: 31`
+				);
+			}
+		}
 
-    const result = [
-      this.always,
-      ...Object.entries(this.createdVariants).map(([variantType, variant]) => {
-        if (variantType in chosenVariants) {
-          const chosenVariantKey = chosenVariants[variantType];
-          if (chosenVariantKey in variant) {
-            return variant[chosenVariantKey];
-          } else {
-            throw new Error(
-              `Error: chosen variant key "${chosenVariantKey}" does not exist in variant type "${variantType}". File: variance.ts, Line: 50`
-            );
-          }
-        }
-        return variant.default;
-      }),
-    ];
+		const result = [
+			this.always,
+			...Object.keys(this.createdVariants).map((variantType) => {
+				const variant = this.createdVariants[variantType];
+				if (variantType in chosenVariants) {
+					const chosenVariantKey = chosenVariants[variantType];
+					if (chosenVariantKey in variant) {
+						return variant[chosenVariantKey];
+					} else {
+						throw new Error(
+							`Error: chosen variant key "${chosenVariantKey}" does not exist in variant type "${variantType}". File: variance.ts, Line: 50`
+						);
+					}
+				}
+				return variant.default;
+			}),
+		];
 
-    return twMerge(result.join(" "));
-  }
+		return twMerge(result.join(" "));
+	}
 }
